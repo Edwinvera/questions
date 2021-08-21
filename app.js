8c20ff28727eeabc53f0138566064c6659e35098
edwinvera <edwinverafitness@gmail.com>
2021-08-21 16:06:01 -0400

    Finish JS

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-const buttons = document.querySelectorAll(".question-btn")
+//using selectors inside the element
 
-buttons.forEach(function (btn) {
-  btn.addEventListener("click", function (event) {
-    const question = event.currentTarget.parentElement.parentElement
-    question.classList.toggle("show-text")
+const questions = document.querySelectorAll(".question")
+
+questions.forEach(function (element) {
+  const btn = element.querySelector(".question-btn")
+  
+  btn.addEventListener("click", function () {
+    questions.forEach(function (item) {
+      if (item !== element) {
+        item.classList.remove("show-text")
+      }
+    })
+    element.classList.toggle("show-text")
   })
 })
+
+//traversing the dom
+
+// const buttons = document.querySelectorAll(".question-btn")
+
+// buttons.forEach(function (btn) {
+//   btn.addEventListener("click", function (event) {
+//     const question = event.currentTarget.parentElement.parentElement
+//     question.classList.toggle("show-text")
+//   })
+// })
