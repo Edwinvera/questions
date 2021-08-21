3982c657a39137fdbb00d751049c110552ada4df
edwinvera <edwinverafitness@gmail.com>
2021-08-21 15:50:04 -0400

    add JS

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-//using selectors inside the element
-// traversing the dom
+const buttons = document.querySelectorAll(".question-btn")
+
+buttons.forEach(function (btn) {
+  btn.addEventListener("click", function (event) {
+    const question = event.currentTarget.parentElement.parentElement
+    question.classList.toggle("show-text")
+  })
+})
